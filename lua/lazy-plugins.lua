38eeec4adf1d0d82b02047be8cf10cc8076c1b69
gabriel-antonelli <gabrielantonelli25@gmail.com>
2024-10-31 17:44:56 -0300

    added copilot

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -48,6 +48,7 @@ require('lazy').setup({
   require 'custom/plugins/catppuccin',
   require 'custom/plugins/toggleterm',
   require 'custom/plugins/bufferline',
+  require 'custom.plugins.copilot',
 
   -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
