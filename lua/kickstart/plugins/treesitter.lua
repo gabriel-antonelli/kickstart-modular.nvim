c9d60e7204e0fb4715ebdba9d33463af72dc3398
gabriel-antonelli <gabrielantonelli25@gmail.com>
2026-05-04 18:22:16 -0300

    fix issues with treesitter in version 0.12

--- lua/kickstart/plugins/treesitter.lua
+++ lua/kickstart/plugins/treesitter.lua
@@ -2,30 +2,22 @@ return {
   { -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
-    main = 'nvim-treesitter.configs', -- Sets main module to use for opts
-    -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
+    branch = 'main',
+    config = function()
+      -- Na branch 'main', usamos o instalador diretamente
+      local configs = require 'nvim-treesitter.install'
+
+      -- Configurações de instalação
+      configs.prefer_git = true
+
+      -- Para garantir que os parsers sejam instalados
+      -- O comando TSInstall ainda funciona manualmente
+    end,
+    -- Como a branch main não tem mais o módulo .configs,
+    -- passamos as opções de instalação e deixamos o highlight nativo agir
     opts = {
       ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
-      -- Autoinstall languages that are not installed
       auto_install = true,
-      highlight = {
-        enable = true,
-        -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
-        --  If you are experiencing weird indenting issues, add the language to
-        --  the list of additional_vim_regex_highlighting and disabled languages for indent.
-        additional_vim_regex_highlighting = { 'ruby' },
-      },
-      indent = { enable = true, disable = { 'ruby' } },
     },
-    fold = {
-      enable = true,
-    },
-    -- There are additional nvim-treesitter modules that you can use to interact
-    -- with nvim-treesitter. You should go explore a few and see what interests you:
-    --
-    --    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
-    --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
-    --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
   },
 }
--- vim: ts=2 sts=2 sw=2 et
